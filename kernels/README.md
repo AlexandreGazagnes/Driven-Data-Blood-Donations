076ac638dbdc7f4b9f6ec4eee986a6af912f6d1a
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-07-19 00:03:14 +0200

    finish first_naive model.py

---
+++ kernels/README.md
@@ -0,0 +1,6 @@
+00 - first_tour.py
+01 - first_naives_model.py
+02 - improving_naives_model.py
+03 - feature_engineering.py
+04 - advanced_models.py
+05 - ....
--- kernels/first_naive_model.py
+++ kernels/first_naive_model.py
@@ -9,20 +9,40 @@
 
 
 """
+find here a first study of the dataset, in which we seek to understand and
+give meaning to the dataset.
+
+we are not trying to solve our problem but will focus on visualization,
+clenaning and feature engineering.
+
+at first we will just study the corelations, the links, the quality and the
+meaning of our dataset.
+
+external research and more general considerations may be included in this work
 
 """
 
 
+
 # import
 
-# from sklearn.grid_search import *
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.dummy import DummyClassifier
 # from sklearn.preprocessing import *
+from sklearn.model_selection import train_test_split
+# from sklearn.grid_search import *
+
+from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
 
+from sklearn.metrics import accuracy_score
+
 from first_tour import *
+
+
+
+# consts 
+
+# COLUMNS = ["naive", "dummy", "basic", "features eng."]
+# MODELS = [naive_model, dummy_model, basic_model]
 
 
 # functions
@@ -37,15 +57,15 @@ def return_X_y(df) :
 	return X, y  
 
 
-def naive_model(y) : 
+def naive_model(X_train, X_test, y_train, y_test) : 
 	""" """
 
-	freq = y.value_counts() / len(y)
+	freq = y_test.value_counts() / len(y_test)
 		
-	pred = np.random.binomial(1, freq[1], len(y))
-	pred = pd.Series(pred)
+	y_pred = np.random.binomial(1, freq[1], len(y_test))
+	y_pred = pd.Series(y_pred)
 
-	return accuracy_score(y, pred)
+	return accuracy_score(y_test, y_pred).round(3)
 
 
 def split(X,y) : 
@@ -64,7 +84,7 @@ def dummy_model(X_train, X_test, y_train, y_test) :
 	model.fit(X_train, y_train)
 	y_pred = model.predict(X_test)
 
-	return accuracy_score(y_test, y_pred)
+	return accuracy_score(y_test, y_pred).round(3)
 
 
 def basic_model(X_train, X_test, y_train, y_test) : 
@@ -74,13 +94,80 @@ def basic_model(X_train, X_test, y_train, y_test) :
 	model.fit(X_train, y_train)
 	y_pred = model.predict(X_test)
 
-	return accuracy_score(y_test, y_pred)
+	return accuracy_score(y_test, y_pred).round(3)
 
 
-# df = first_tour()
-# X,y = return_X_y(df)
-# t = split(X,y)
+def model_accuracy_mean(model, nb, X_train, X_test, y_train, y_test) : 
+	""" """
 
-# print(naive_model(y))
+	scores = [model(X_train, X_test, y_train, y_test) for i in range(nb)]
 
-# print(basic_model(*t))
+	score = sum(scores)/len(scores)
+
+	return score.round(3)
+
+
+
+#############################################################
+
+COLUMNS = ["naive", "dummy", "basic", "features eng."]
+MODELS = [naive_model, dummy_model, basic_model]
+
+##############################################################
+
+
+
+
+def add_new_results(results, feat_com, t, n=100, models=MODELS, columns= COLUMNS) : 
+	""" """
+	
+	new = [model_accuracy_mean(i, n, *t) for i in models]
+	info(new)
+
+	new.append(feat_com)
+	info(new)
+	
+	new = pd.Series(new, index=columns)
+	info(new)
+	
+	results = results.append(new, ignore_index=True)
+	info(results)
+
+	return results
+
+
+def first_approch_of_feat_eng(results, drop_list) : 
+	""" """
+	
+	if not isinstance(drop_list, list) : 
+		raise TypeError
+
+	for i in drop_list : 
+		df = first_tour()
+		df = delete_outliers(df, i) 
+		X,y = return_X_y(df)
+		t = split(X,y)
+		comment = "drop outliers > " + str(i)
+		results = add_new_results(results, comment,t)
+
+	return results
+
+
+def first_naive_model() : 
+	""" """	
+	df = first_tour()
+	X,y = return_X_y(df)
+	t = split(X,y)
+
+	results = pd.DataFrame(columns=COLUMNS)
+	results = add_new_results(results, "without_any_feat_eng", t)
+
+	results = first_approch_of_feat_eng(results, [1.5, 2.0, 2.5, 3.0, 3.5])
+	
+	return results
+
+
+
+# results = first_naive_model()
+
+
